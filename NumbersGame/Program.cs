7eb17e13d3dcbea895a7b9a260bc271e8b23a377
Huan <huanyang.ooi@gmail.com>
2023-09-28 17:54:38 +0200

    Add project files.

---
+++ NumbersGame/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace NumbersGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Välkommen! Jag tänker på ett nummer. Kan du gissa vilket? Du får fem försök.");
+            int[] test5 = { 5, 6, 7 };
+        
+        }
+    }
+}
